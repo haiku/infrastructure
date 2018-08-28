a0d7dc73971bc63b517d9aec1621fe6f4ea8ed26
Alexander von Gluck IV <kallisti5@unixzen.com>
2018-08-28 08:33:58 -0500

    buildbot: Fine, we'll make our own secret storage.

--- docker/buildbot-master/start_buildbot.sh
+++ docker/buildbot-master/start_buildbot.sh
@@ -4,8 +4,12 @@
 # simplified and modified to workaround missing support for secrets with GitHubAuth
 
 B=`pwd`
+
+#https://github.com/buildbot/buildbot/issues/4137
 GITHUB_CLIENT=$(cat $B/secrets/github-auth-client)
 GITHUB_SECRET=$(cat $B/secrets/github-auth-secret)
+#https://github.com/buildbot/buildbot/issues/3627
+DATABASE_URI=$(cat $B/secrets/database)
 
 if [ ! -f $B/buildbot.tac ]
 then
@@ -15,6 +19,7 @@ fi
 cp $B/master.cfg $B/master.cfg.secret
 sed -i -e "s/@@GITHUB_CLIENT@@/$GITHUB_CLIENT/" $B/master.cfg.secret
 sed -i -e "s/@@GITHUB_SECRET@@/$GITHUB_SECRET/" $B/master.cfg.secret
+sed -i -e "s/@@DATABASE_URI@@/$DATABASE_URI/" $B/master.cfg.secret
 sed -i -e "s/'master.cfg'/'master.cfg.secret'/" $B/buildbot.tac
 
 rm -f $B/twistd.pid
