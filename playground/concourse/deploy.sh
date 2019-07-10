7f515454984d37ff116960aba2686eab62540026
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-07-10 15:05:05 -0500

    concourse: Fix nightly pipeline to use toolchain-worker container

--- playground/concourse/deploy.sh
+++ playground/concourse/deploy.sh
@@ -2,8 +2,10 @@
 
 # Create-team also logs in.
 ./create-team.sh test continuous
+./create-team.sh test nightly
 ./create-team.sh test r1beta1
 
 # Deploy pipelines
-./apply-pipeline.sh continuous master secrets.source
-./apply-pipeline.sh r1beta1 r1beta1 secrets.source
+./apply-pipeline.sh continuous master minimal secrets.source
+./apply-pipeline.sh nightly r1beta1 nightly secrets.source
+./apply-pipeline.sh r1beta1 r1beta1 release secrets.source
