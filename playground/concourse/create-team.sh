c15b338ca3898d347e9455393c7406946aadb0fd
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-07-20 11:53:50 -0500

    concourse: Ensure teams get the correct permissions

--- playground/concourse/create-team.sh
+++ playground/concourse/create-team.sh
@@ -11,5 +11,5 @@ TEAM="$1"
 . $2
 
 $FLY_CLI login -t main -c $CONCOURSE_URL -n $FLY_LOCAL_TEAM -b -u $FLY_LOCAL_USERNAME -p $FLY_LOCAL_PASSWORD
-$FLY_CLI -t main set-team -n $TEAM --local-user=$FLY_LOCAL_USERNAME --non-interactive
+$FLY_CLI -t main set-team -n $TEAM --github-team=haiku:infrastructure --local-user=$FLY_LOCAL_USERNAME --non-interactive
 $FLY_CLI login -t $TEAM -c $CONCOURSE_URL -n $TEAM -b -u $FLY_LOCAL_USERNAME -p $FLY_LOCAL_PASSWORD
