ec26b9d3b53cb66de900f5338ac0f60640751179
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-10-17 16:33:06 -0500

    concourse: Cleanup deployment process, skip local admin user
    * This assumes the concourse server has had the initial teams pre-created
      and you're an owner of each.
    * Now uses a single target, and switches the target team for each action.
    * TODO: create a bootstrap script

--- playground/concourse/apply-pipeline.sh
+++ playground/concourse/apply-pipeline.sh
@@ -16,6 +16,19 @@ fi
 
 FLY_CLI=fly
 
+echo "Switching haiku target to $TEAM team..."
+$FLY_CLI -t haiku edit-target -n $TEAM
+if [ $? -ne 0 ]; then
+	echo "Error setting haiku target to $TEAM team!"
+	exit 1
+fi
+
+$FLY_CLI -t haiku status
+if [ $? -ne 0 ]; then
+	echo "Error setting haiku target to $TEAM team!"
+	exit 1
+fi
+
 if [ $BRANCH == "r1beta1" ]; then
 	BUCKET="haiku-release-test"
 	BRANCH_PROFILE="release"
@@ -35,8 +48,8 @@ fi
 
 if [ "$TEAM" != "continuous" ]; then
 	echo "Deploy toolchain builder..."
-	$FLY_CLI -t $TEAM set-pipeline -n -p toolchain-$BRANCH -v branch=$BRANCH -l $SECRETS -c pipelines/toolchain-builder.yml
-	$FLY_CLI -t $TEAM expose-pipeline -p toolchain-$BRANCH
+	$FLY_CLI -t haiku set-pipeline -n -p toolchain-$BRANCH -v branch=$BRANCH -l $SECRETS -c pipelines/toolchain-builder.yml
+	$FLY_CLI -t haiku expose-pipeline -p toolchain-$BRANCH
 fi
 
 # Anyboot is the "ideal image media type"
@@ -54,9 +67,9 @@ for ARCH in $ARCHES; do
 	echo "Applying $BRANCH - $ARCH target: @$PROFILE-$TYPE"
 
 	if [ "$TEAM" == "continuous" ]; then
-		$FLY_CLI -t $TEAM set-pipeline -n -p $BRANCH-$ARCH -v branch=$BRANCH -v arch=$ARCH -l $SECRETS -v profile=$PROFILE -v media=$MEDIA -c pipelines/haiku-continuous.yml
+		$FLY_CLI -t haiku set-pipeline -n -p $BRANCH-$ARCH -v branch=$BRANCH -v arch=$ARCH -l $SECRETS -v profile=$PROFILE -v media=$MEDIA -c pipelines/haiku-continuous.yml
 	else
-		$FLY_CLI -t $TEAM set-pipeline -n -p $BRANCH-$ARCH -v branch=$BRANCH -v arch=$ARCH -l $SECRETS -v profile=$PROFILE -v media=$MEDIA -v bucket=$BUCKET -y days=\[$DAYS\] -c pipelines/haiku-release.yml
+		$FLY_CLI -t haiku set-pipeline -n -p $BRANCH-$ARCH -v branch=$BRANCH -v arch=$ARCH -l $SECRETS -v profile=$PROFILE -v media=$MEDIA -v bucket=$BUCKET -y days=\[$DAYS\] -c pipelines/haiku-release.yml
 	fi
-	$FLY_CLI -t $TEAM expose-pipeline -p $BRANCH-$ARCH
+	$FLY_CLI -t haiku expose-pipeline -p $BRANCH-$ARCH
 done
--- playground/concourse/deploy.sh
+++ playground/concourse/deploy.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]; then
-	echo "Usage: $0 <fly_secrets> <secrets>"
+CONCOURSE_URL="https://ci.haiku-os.org"
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage: $0 <secrets>"
 	exit 1
 fi
 
-# Create-team also logs in.
-fly -t continuous status
-if [[ $? -ne 0 ]]; then
-	./create-team.sh continuous $1
+# Logout if we're logged in
+fly -t haiku status
+if [[ $? -eq 0 ]]; then
+	echo "Logging out old session..."
+	fly -t haiku delete-target
 fi
 
-fly -t nightly status
-if [[ $? -ne 0 ]]; then
-	./create-team.sh nightly $1
-fi
+# Login to the continious team
+fly -t haiku login -c $CONCOURSE_URL -n continuous -b
 
-fly -t r1beta1 status
-if [[ $? -ne 0 ]]; then
-	./create-team.sh r1beta1 $1
-fi
+## Deploy pipelines
+## continuous team.  Builds on every commit. Artifacts not saved
+fly -t haiku set-team -n continuous --github-team=haiku:infrastructure --non-interactive
+./apply-pipeline.sh continuous master $1
 
-# Deploy pipelines
-./apply-pipeline.sh continuous master $2
-./apply-pipeline.sh nightly master $2
-./apply-pipeline.sh r1beta1 r1beta1 $2
+## nightly team.  Builds releases every night. Artifacts pushed to nightly bucket
+fly -t haiku set-team -n nightly --github-team=haiku:infrastructure --non-interactive
+./apply-pipeline.sh nightly master $1
+
+## r1beta1 team. Builds released weekly. Artifacts pushed to release buckets
+fly -t haiku set-team -n r1beta1 --github-team=haiku:infrastructure --non-interactive
+./apply-pipeline.sh r1beta1 r1beta1 $1
