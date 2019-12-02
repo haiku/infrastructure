b8bcf9ff69191f54ea60e0611340dcd47e5dc134
Alexander von Gluck IV <kallisti5@unixzen.com>
2019-12-02 08:48:06 -0600

    concourse: Move to mc for image uploads as well
    * The resource-s3 in concourse is trash tbh.
    * It forces you to do versioned artifacts which doesn't
      make sense for us.
    * If you're not using versioned artifacts, it demands
      versioning via regex. If something in the bucket isn't
      named aligning with the regex it fails with a obtuse error.
    * Just use mc until the situation improves.

--- concourse/apply-pipeline.sh
+++ concourse/apply-pipeline.sh
@@ -30,17 +30,20 @@ if [ $? -ne 0 ]; then
 fi
 
 if [ $BRANCH == "r1beta1" ]; then
-	BUCKET="haiku-release-test"
+	BUCKET_IMAGE="haiku-release-candidates"
+	BUCKET_REPO="haiku-repositories-test"
 	BRANCH_PROFILE="release"
 	ARCHES="x86_64 x86_gcc2h"
 	DAYS="Sunday"
 elif [ $BRANCH == "r1beta2" ]; then
-	BUCKET="haiku-release-test"
+	BUCKET_IMAGE="haiku-release-candidates"
+	BUCKET_REPO="haiku-repositories-test"
 	BRANCH_PROFILE="release"
 	ARCHES="x86_64 x86_gcc2h"
 	DAYS="Monday,Friday"
 else
-	BUCKET="haiku-nightly-test"
+	BUCKET_IMAGE="haiku-nightly"
+	BUCKET_REPO="haiku-repositories-test"
 	BRANCH_PROFILE="nightly"
 	ARCHES="x86_64 x86_gcc2h arm sparc riscv64 ppc m68k"
 	DAYS="Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday"
@@ -69,7 +72,7 @@ for ARCH in $ARCHES; do
 	if [ "$TEAM" == "continuous" ]; then
 		$FLY_CLI -t haiku set-pipeline -n -p $BRANCH-$ARCH -v branch=$BRANCH -v arch=$ARCH -l $SECRETS -v profile=$PROFILE -v media=$MEDIA -c pipelines/haiku-continuous.yml
 	else
-		$FLY_CLI -t haiku set-pipeline -n -p $BRANCH-$ARCH -v branch=$BRANCH -v arch=$ARCH -l $SECRETS -v profile=$PROFILE -v media=$MEDIA -v bucket=$BUCKET -y days=\[$DAYS\] -c pipelines/haiku-release.yml
+		$FLY_CLI -t haiku set-pipeline -n -p $BRANCH-$ARCH -v branch=$BRANCH -v arch=$ARCH -l $SECRETS -v profile=$PROFILE -v media=$MEDIA -v bucket_image=$BUCKET_IMAGE -v bucket_repo=$BUCKET_REPO -y days=\[$DAYS\] -c pipelines/haiku-release.yml
 	fi
 	$FLY_CLI -t haiku expose-pipeline -p $BRANCH-$ARCH
 done
