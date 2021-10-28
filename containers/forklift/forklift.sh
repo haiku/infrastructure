2cc0024d011c8e55c4174d4695c03587f5c2860d
Alexander von Gluck IV <kallisti5@unixzen.com>
2021-10-28 10:58:32 -0500

    forklift: Fix typo in build-packages path check

--- containers/forklift/forklift.sh
+++ containers/forklift/forklift.sh
@@ -9,22 +9,26 @@ export OUTGOING=/haikuports-data/build-packages/master/packages
 # build-packages
 while true; do
 	for i in arm arm64 m68k ppc sparc riscv64 x86 x86_64; do
-		if [ ! -d "${INCOMING}/build-packages/${i})" ]; then
-			echo "$(date) - build-packages/$i missing!"
+		ARCH_PATH="${INCOMING}/build-packages/${i}"
+		if [ ! -d "${ARCH_PATH}" ]; then
+			echo "$(date) - ${ARCH_PATH} missing!"
 			continue;
 		fi
-		if [ ! "$(ls -A ${INCOMING}/build-packages/${i})" ]; then
+		if [ ! "$(ls -A ${ARCH_PATH})" ]; then
 			continue;
 		else
 			echo "$(date) - new build-packages inbound for $i..."
-			ls -la ${INCOMING}/build-packages/${i}
+			ls -la ${ARCH_PATH}
 			# clobber architecture packages
-			find ${INCOMING}/build-packages/${i} -name "*-${i}.hpkg" -mmin +30 -exec mv -v -f {} ${OUTGOING}/ \;
+			echo "$(date) - moving $i packages to build-packages repo..."
+			find ${ARCH_PATH} -name "*-${i}.hpkg" -mmin +30 -exec mv -v -f {} ${OUTGOING}/ \;
 			# don't clobber source or any packages since it could break other architecture repos?
-			find ${INCOMING}/build-packages/${i} -name "*-source.hpkg" -mmin +30 -exec mv -v -n {} ${OUTGOING}/ \;
-			find ${INCOMING}/build-packages/${i} -name "*-any.hpkg" -mmin +30 -exec mv -v -n {} ${OUTGOING}/ \;
+			echo "$(date) - moving $i source/any packages to build-packages repo..."
+			find ${ARCH_PATH} -name "*-source.hpkg" -mmin +30 -exec mv -v -n {} ${OUTGOING}/ \;
+			find ${ARCH_PATH} -name "*-any.hpkg" -mmin +30 -exec mv -v -n {} ${OUTGOING}/ \;
 			# cleanup
-			find ${INCOMING}/build-packages/${i} -name "*" -mmin +60 -exec rm -vf {} \;
+			echo "$(date) - cleaning up anything we don't need / want..."
+			find ${ARCH_PATH} -name "*" -mmin +60 -exec rm -vf {} \;
 		fi
 	done
 	sleep 5m
