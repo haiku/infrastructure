332512a6043d30d60a09a2f656181e816d2d514b
Alexander von Gluck IV <kallisti5@unixzen.com>
2021-10-28 10:51:51 -0500

    forklift: add some maintenance tasks, deleting things that don't belong

--- containers/forklift/forklift.sh
+++ containers/forklift/forklift.sh
@@ -4,17 +4,26 @@
 # in the future.. but it seems better today to do it manually
 
 export INCOMING=/incoming
-export OUTGOING=/haikuports-data/build-packages/master/packages/
+export OUTGOING=/haikuports-data/build-packages/master/packages
 
 # build-packages
 while true; do
 	for i in arm arm64 m68k ppc sparc riscv64 x86 x86_64; do
+		if [ ! -d "${INCOMING}/build-packages/${i})" ]; then
+			echo "$(date) - build-packages/$i missing!"
+			continue;
+		fi
 		if [ ! "$(ls -A ${INCOMING}/build-packages/${i})" ]; then
 			continue;
 		else
-			echo "new build-packages inbound for $i..."
+			echo "$(date) - new build-packages inbound for $i..."
 			ls -la ${INCOMING}/build-packages/${i}
-			find ${INCOMING}/build-packages/${i} -name "*.hpkg" -mmin +15 -exec mv -v -f {} ${OUTGOING}/ \;
+			# clobber architecture packages
+			find ${INCOMING}/build-packages/${i} -name "*${i}.hpkg" -mmin +30 -exec mv -v -f {} ${OUTGOING}/ \;
+			# don't clobber source packages since it could break other architecture repos?
+			find ${INCOMING}/build-packages/${i} -name "*source.hpkg" -mmin +30 -exec mv -v -n {} ${OUTGOING}/ \;
+			# cleanup
+			find ${INCOMING}/build-packages/${i} -name "*" -mmin +60 -exec rm -vf {} \;
 		fi
 	done
 	sleep 5m
