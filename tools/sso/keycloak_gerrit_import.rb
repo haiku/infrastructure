ea87d7a96be5bea7bd0a34fd05710e1860535572
Alexander von Gluck IV <kallisti5@unixzen.com>
2023-06-08 12:44:41 -0500

    tools/sso: Fix keycloak gerrit import server domain

--- tools/sso/keycloak_gerrit_import.rb
+++ tools/sso/keycloak_gerrit_import.rb
@@ -13,7 +13,7 @@ end
 KeycloakAdmin.configure do |config|
 	config.use_service_account = false
 	config.server_url            = "https://sso.haiku-os.org"
-	config.server_domain	     = "auth.haiku-os.org"
+	config.server_domain	     = "sso.haiku-os.org"
 	config.client_id	         = "admin-cli"
 	config.client_realm_name	 = "master"
 	config.use_service_account = false
@@ -23,8 +23,12 @@ KeycloakAdmin.configure do |config|
 	config.rest_client_options = { verify_ssl: OpenSSL::SSL::VERIFY_NONE }
 end
 
+# Developers:
 # curl -s --header "Content-Type: application/json" --user "username:password" https://review.haiku-os.org/a/groups/groupid/members | grep -v ")]}'" > ~/gerrit-developers.json
 #
+# All active users:
+# for i in $(curl -s --header "Content-Type: application/json" --user "username:password" https://review.haiku-os.org/a/accounts/?q=is:active | grep -v ")]}'" | jq | grep _account_id | cut -d':' -f2); do curl -s --header "Content-Type: application/json" --user "username:password" https://review.haiku-os.org/a/accounts/$i | grep -v ")]}'" >> ~/gerrit-users.json; done
+
 data = File.read(ARGV.first)
 users = JSON.parse(data)
 
@@ -36,16 +40,20 @@ users.each do |user|
 	username = user["username"]
 	email = user["email"]
 	password = (0...128).map { ('a'..'z').to_a[rand(26)] }.join
-	email_verified = false
+	email_verified = true
 
-	puts "WILL ADD #{username}, #{email}, #{password}..."
+    if email == nil or email.length <= 3
+      puts "SKIP: #{username} (#{firstname} #{lastname}) missing email! Skipping..."
+      next
+    end
+
+	puts "ADD: #{username}, #{email}, #{password}..."
 	begin
 		KeycloakAdmin.realm("haiku").users.create!(username, email, password, email_verified, "en")
 	rescue RuntimeError => error
-		puts "FAIL - Unable to add #{username} - #{error}"
+		puts "FAIL: Unable to add #{username} - #{error}"
 		next
 	end
-	puts "SUCCESS - Added #{username} - #{email}..."
 
 	update_name = Hash.new
 	update_name.merge!({firstName: firstname}) if firstname && firstname.length > 0
@@ -53,11 +61,9 @@ users.each do |user|
 
 	begin
 		user = KeycloakAdmin.realm("haiku").users.search(email).first
-		puts("found #{user.id} to update with...")
-		pp update_name
-		pp KeycloakAdmin.realm("haiku").users.update(user.id, update_name)
+		KeycloakAdmin.realm("haiku").users.update(user.id, update_name)
 	rescue RuntimeError => error
-		puts "FAIL - Unable to add #{username} - #{error}"
+		puts "FAIL: Unable to update name of #{username} to #{user["name"]}: #{error}"
 		next
 	end
 end
