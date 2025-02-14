54e5dcc3957423389ed149bb782f5aaca342a652
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2025-02-14 07:38:36 +0000

    Pootle: add all Chinese languages to the post-processing rule

--- containers/services/pootle/scripts/finish_output_catalogs.py
+++ containers/services/pootle/scripts/finish_output_catalogs.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Clean up the output catalogs and calculate the fingerprint.')
 parser.add_argument('pootle_catalogs_dir', metavar='input_catalogs', type=str,
                    help='the location of the input catalogs')
-parser.add_argument('repository_catalogs_dir', metavar="output_catalogs", 
+parser.add_argument('repository_catalogs_dir', metavar="output_catalogs",
                    type=str, help='the location of the output catalogs')
 parser.add_argument('language_list', metavar="languages", nargs="+",
                     type=str, help='a list of languages that you want to export')
@@ -31,14 +31,14 @@ for language in args.language_list:
     for catalog in source_list:
         f = open(os.path.join(args.pootle_catalogs_dir, catalog), "r")
         lines = f.readlines()
-    
+
         catalogentries = []
         for line in lines[1:]:
             string, context, comment, translation = line.split('\t')
             # drop empty lines
             if translation.strip() != "":
                 catalogentries.append((string, context, comment, translation))
-    
+
         if len(catalogentries) == 0:
             # this file is not translated
             f.close()
@@ -46,7 +46,7 @@ for language in args.language_list:
 
         catalog_count += 1
         translated_count += len(catalogentries)
-    
+
         calculated_fingerprint = computefingerprint(catalogentries)
         header = lines[0].rsplit('\t', 1)[0] + '\t' + str(calculated_fingerprint) + '\n'
 
@@ -54,15 +54,15 @@ for language in args.language_list:
             print("WARNING: copying a catkeys file to non-existent target directory. Is this right?")
             print(catalog)
             os.makedirs(os.path.split(os.path.join(args.repository_catalogs_dir, catalog))[0])
-    
-    # HACK: ICU uses zh_Hans insteald of zh-Hans
-        if 'zh-Hans' in catalog:
-            catalog = catalog.replace('zh-Hans', 'zh_Hans')
+
+    # HACK: ICU uses zh_Hans/zh_Hant instead of zh-Hans/zh-Hant
+        if 'zh-Han' in catalog:
+            catalog = catalog.replace('zh-Han', 'zh_Han')
         output_f = open(os.path.join(args.repository_catalogs_dir, catalog), "w")
         output_f.write(header)
         for entry in catalogentries:
             output_f.write("%s\t%s\t%s\t%s" % (entry[0], entry[1], entry[2], entry[3]))
-    
+
         f.close()
         output_f.close()
 
